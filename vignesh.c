c110bd487f7371e3ddf80c2163183e516f30c9ec
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-02-07 16:45:46 +0530

    Create vignesh.c

---
+++ vignesh.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,i;
+printf("enter the number");
+scanf("%d",&a);
+for(i=0;i<1000;i++)
+{
+printf("%d\n",b);
+scanf("%d",&b);
+{
+if(a%b=0 && a%1=0)
+printf("yes");
+}
+}
+else 
+printf("no");
+getch();
+return 0;
+}
+
