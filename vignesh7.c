b161c1a320208e554f52faaee0aea47440d82002
mechmadam <35689265+mechmadam@users.noreply.github.com>
2018-02-07 16:38:31 +0530

    Create vignesh7.c

---
+++ vignesh7.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c,d,e;
+printf("enter the number");
+scanf("%d",&a);
+b=a/100;
+c=(a-(1*100))/10;
+d=(a-(1*100+c*10);
+e=(d*100+c*10+b*1);
+if(a=e);
+printf("%d its palindrome");
+else
+printf("%d is not an palindrome);
+getch();
+return 0;
+}
